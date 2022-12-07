fd99ec7d6748e144f154f401813978676648cfad
kayzed <kayzed98@gmail.com>
2022-12-07 16:37:11 -0500

    Add reset game btn, place mark and new game funcs

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const gameboard = (() => {
-    const gameboardDiv = document.querySelector(".gameboardDiv");
+    const gameboardGrid = document.querySelector(".gameboardGrid");
+    const newGameBtn = document.querySelector(".newGameBtn");
     const x = "images/x.svg";
     const o = "images/circle.svg"
     const arr = [x, o, x, x, o, x, x, o, x];
     const obj = {arr,};
+    let turn = true;
     const makeBoard = () => {
         for (let i= 1; i <= 9; i++){
            let square = document.createElement("div");
@@ -11,8 +13,23 @@ const gameboard = (() => {
            if (i == 2 || i == 5 || i == 8){square.classList.add("midV")};
            if (i == 4 || i == 5 || i == 6){square.classList.add("midH")}; 
            square.setAttribute("data-square", `${i}`);
-           gameboardDiv.appendChild(square);
+           square.addEventListener("click", (e) => placeMark(e), {once: true})
+           gameboardGrid.appendChild(square);
         }
+    }
+    const placeMark = (e) => {
+        console.log(e.target);
+        let targetSquare = e.target;
+        let imgTag = document.createElement("img");
+        if (turn == true) {
+            imgTag.setAttribute("src",x);
+            turn = false
+        }
+        else if (turn == false){
+            imgTag.setAttribute("src", o);
+            turn = true;
+        }
+        targetSquare.appendChild(imgTag);
     }
     const fillBoard = () => {
         for (let i=1; i <= arr.length; i++){
@@ -23,11 +40,17 @@ const gameboard = (() => {
             square.appendChild(imgTag)
         }
     }
-    return {obj, makeBoard, fillBoard}
+    const newGame = () => {
+        while (gameboardGrid.childNodes.length > 0){
+            gameboardGrid.removeChild(gameboardGrid.lastChild)
+        }
+        makeBoard();
+    }
+    newGameBtn.addEventListener("click", newGame);
+    makeBoard()
+    return {gameboardGrid}
 })()
 
-gameboard.makeBoard()
-gameboard.fillBoard()
 
 const createPlayer = (name) => {
     let player = {
