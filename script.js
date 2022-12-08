6f222904d356887fcc8fbe02f57526558419f374
kayzed <kayzed98@gmail.com>
2022-12-07 21:18:22 -0500

    Add create player functions, returns private obj

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
+const Players = () => {
+    const playerOne = createPlayer(document.querySelector("#playerOne").value);
+    const playerTwo = createPlayer(document.querySelector("#playerTwo").value);
+    function createPlayer(name) {
+        let player = {
+            name,
+            wins: 0,
+            addWin(){
+                this.wins += 1
+            }
+        }
+        return player 
+    }
+   return {playerOne, playerTwo}
+}
+
 const gameboard = (() => {
     const gameboardGrid = document.querySelector(".gameboardGrid");
     const newGameBtn = document.querySelector(".newGameBtn");
-    const x = "images/x.svg";
-    const o = "images/circle.svg"
+    newGameBtn.addEventListener("click", _newGame);
+    newGameBtn.addEventListener("click", createPlayers, {once: true})
     let boardArr = [1,2,3,4,5,6,7,8,9];
-    let boardObj = {boardArr};
     let turn = true;
-    const makeBoard = () => {
+    let players;
+    function createPlayers(){
+        players = Players();
+    }
+    function _makeBoard() {
         for (let i= 1; i <= 9; i++){
            let square = document.createElement("div");
            square.classList.add("square");
            if (i == 2 || i == 5 || i == 8){square.classList.add("midV")};
            if (i == 4 || i == 5 || i == 6){square.classList.add("midH")}; 
            square.setAttribute("data-square", `${i}`);
-           square.addEventListener("click", (e) => placeMark(e), {once: true})
+           square.addEventListener("click", (e) => _placeMark(e), {once: true})
            gameboardGrid.appendChild(square);
         }
     }
-    const placeMark = (e) => {
+    const _placeMark = (e) => {
+        const x = "images/x.svg";
+        const o = "images/circle.svg"
         let targetSquare = e.target;
         let targetDataSquare = e.target.getAttribute("data-square");
         let imgTag = document.createElement("img");
@@ -32,16 +53,17 @@ const gameboard = (() => {
             turn = true;
         }
         targetSquare.appendChild(imgTag);
-        checkWin();
+        _checkWin();
     }
-    const newGame = () => {
+    function _newGame() {
         while (gameboardGrid.childNodes.length > 0){
             gameboardGrid.removeChild(gameboardGrid.lastChild)
         }
         boardArr = [1,2,3,4,5,6,7,8,9];
-        makeBoard();
+        turn = true;
+        _makeBoard();
     }
-    function checkWin(){
+    function _checkWin(){
         let arr = boardArr;
         const combos = [arr.slice(0,3),arr.slice(3,6),arr.slice(6,9),
                         [0,3,6].map(x => arr[x]),[1,4,7].map(x => arr[x]),
@@ -49,21 +71,21 @@ const gameboard = (() => {
                         [2,4,6].map(x => arr[x])];
         for (let i = 0; i < combos.length; i++ ){
             if (combos[i].every(x => x == combos[i][0])){
-                console.log(`win = ${combos[i][0]}`)
+                if(combos[i][0] == "x"){
+                    players.playerOne.addWin();
+                    console.log(players)
+                }
+                else {
+                    players.playerTwo.addWin()
+                    console.log(players)
+                }
             }
         }
     }
-    newGameBtn.addEventListener("click", newGame);
-    makeBoard()
-    return {boardArr}
+    return {
+        newGame: () => {
+            _newGame()
+        }
+    }
 })()
 
-
-const createPlayer = (name) => {
-    let player = {
-        name,
-    }
-    return player
-}
-
-const playerOne = createPlayer("Sam")
